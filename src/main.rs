65f7bb7315979d36d69c9d7e62c19925ca1de39f
Eiyarrr <eiyar@eiyar.com>
2026-04-10 00:35:46 +0000

    Added more base functions for computation

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,32 @@
  *  find row at or below curr with nonzero entry in column
  *  None? move right one
  *  Found? Swap row with curr row
- * Scale so index = 0
+ * Scale so pivot = 1
  * make all other entries in column 0
  * Repeat
  */
 
+const ROWS: usize = 3;
+const COLUMNS: usize = 4;
 
-fn into_ref(matrix: [[f64; 4]; 3]) {}
+// sets all the numbers in a column, aside from the given row, to 0 using the given row
+fn set_columns_to_zero(matrix: [[f64; COLUMNS]; ROWS], row: i32) {}
 
-fn into_rref(matrix: [[f64; 4]; 3]) {}
+// swaps two rows places
+fn swap_rows(matrix: [[f64; COLUMNS]; ROWS], row1: i32, row2: i32) {}
+
+// sets pivot to 1
+fn set_pivot(row: [f64; COLUMNS]) {}
+
+// start function for REF
+fn into_ref(matrix: [[f64; COLUMNS]; ROWS]) {}
+
+// start function for RREF
+fn into_rref(matrix: [[f64; COLUMNS]; ROWS]) {}
 
 fn main() {
     // proper RREF solution is 1, -1, 2
-    let matrix: [[f64; 4]; 3] = [
+    let matrix: [[f64; COLUMNS]; ROWS] = [
         [2.0, -5.0, 5.0, 17.0],
         [0.0, 1.0, 3.0, 5.0],
         [1.0, -2.0, 3.0, 9.0],
